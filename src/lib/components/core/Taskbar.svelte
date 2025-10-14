1794d282ff068a4726a639d53194bea40c1b392e
Szigeti Balázs <balazs.mindenes@gmail.com>
2025-10-14 11:23:06 +0200

    Taskbar és start menü módosítás

--- src/lib/components/core/Taskbar.svelte
+++ src/lib/components/core/Taskbar.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
 	import { Rocket } from 'lucide-svelte';
 	import { getWindowManager } from '$lib/stores/windowStore.svelte';
-	import UniversalIcon from '../UniversalIcon.svelte';
-	import StartMenu from './StartMenu.svelte';
-	import Clock from '../Clock.svelte';
 	import { getThemeManager } from '$lib/stores/themeStore.svelte';
+	import UniversalIcon from '$lib/components/UniversalIcon.svelte';
+	import StartMenu from './StartMenu.svelte';
+	import Clock from '$lib/components/Clock.svelte';
 	import * as Popover from '$lib/components/ui/popover';
+
 	const themeManager = getThemeManager();
 	const windowManager = getWindowManager();
-	let showStartMenu = $state(false);
-	/**
-	 * Toggle start menu visibility
-	 */
-	function toggleStartMenu() {
-		showStartMenu = !showStartMenu;
-	}
 </script>
 
 <!-- Taskbar a minimalizált ablakok számára -->
 <div class="taskbar">
 	<div class="taskbar-left">
 		<Popover.Root>
-			<Popover.Trigger class="btn-startmenu h-full"><Rocket size={24} /></Popover.Trigger>
-			<Popover.Content class="z-[1000] mx-2 my-1 w-96"><StartMenu /></Popover.Content>
+			<Popover.Trigger class="btn-startmenu"><Rocket size={24} /></Popover.Trigger>
+			<Popover.Content
+				class="z-[1000] mx-3 my-2 min-h-[var(--startmenu-height)] w-[var(--startmenu-width)]"
+				><StartMenu /></Popover.Content
+			>
 		</Popover.Root>
 
 		{#each windowManager.windows as window}
@@ -44,7 +41,7 @@
 				}}
 			>
 				<div class="taskbar-item-icon">
-					<UniversalIcon icon={window.icon ?? ''} size={32} appName={window.appName} />
+					<UniversalIcon icon={window.icon ?? ''} size={24} appName={window.appName} />
 				</div>
 				<div class="taskbar-item-title">
 					<span>{window.title}</span>
@@ -58,6 +55,9 @@
 </div>
 
 <style>
+	@reference "tailwindcss";
+	@custom-variant dark (&:is(.dark *));
+
 	.taskbar {
 		display: flex;
 		justify-content: space-between;
@@ -72,7 +72,7 @@
 			display: flex;
 			flex-grow: 1;
 			justify-content: start;
-			align-items: center;
+			align-items: stretch;
 			gap: 5px;
 		}
 
@@ -82,77 +82,74 @@
 			align-items: center;
 			padding-right: 16px;
 		}
+
+		.taskbar-item {
+			display: flex;
+			align-items: center;
+			gap: 10px;
+			transition: all 0.2s;
+			cursor: pointer;
+			margin: 5px 0;
+			border-radius: var(--radius-sm);
+			padding: 0 10px;
+
+			.taskbar-item-title {
+				display: flex;
+				align-items: stretch;
+				max-width: 200px;
+				color: var(--taskbar-item-text-color);
+				font-size: 0.85rem;
+
+				& > span {
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+				}
+			}
+
+			&.active {
+				box-shadow:
+					var(--shadow-sm),
+					0px 15px 20px -15px rgba(from var(--color-primary) r g b / 0.8);
+			}
+
+			&:hover {
+				background-color: var(--color-accent) !important;
+			}
+
+			.taskbar-item-icon {
+				display: flex;
+				align-items: stretch;
+				transition: filter 0.2s ease-in-out;
+			}
+
+			&.minimized {
+				opacity: 0.7;
+				background-color: transparent;
+
+				.taskbar-item-icon {
+					filter: grayscale(1);
+				}
+			}
+		}
 	}
 
 	:global(.btn-startmenu) {
+		@apply mx-3 my-1 h-full;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		transition: background-color 0.2s;
 		cursor: pointer;
+		border-radius: var(--radius-sm);
 		background-color: var(--color-primary-alpha-20);
 		aspect-ratio: 1;
-		&:hover {
-			background-color: var(--color-primary-alpha-80);
-		}
+		height: 45px;
+
+		&:hover,
 		&[data-state='open'] {
 			background-color: var(--color-primary-alpha-80);
-		}
-	}
-
-	.taskbar-item {
-		display: flex;
-		align-items: center;
-		gap: 10px;
-		transition: all 0.2s;
-		cursor: pointer;
-		border: 1px solid transparent;
-		border-radius: 0;
-		background-color: var(--taskbar-item-bg-color);
-		padding: 0 10px;
-
-		height: calc(var(--taskbar-height) - var(--startmenu-margin) - 5px);
-
-		.taskbar-item-icon {
-			display: flex;
-			align-items: center;
-
-			transition: filter 0.2s ease-in-out;
-			height: 100%;
-		}
-		.taskbar-item-title {
-			display: flex;
-			align-items: center;
-			max-width: 200px;
-			height: 100%;
-			color: var(--taskbar-item-text-color);
-			font-size: 0.85rem;
-
-			& > span {
-				overflow: hidden;
-				text-overflow: ellipsis;
-				white-space: nowrap;
-			}
-		}
-	}
-
-	.taskbar-item:hover {
-		background-color: var(--taskbar-item-bg-color_hover) !important;
-	}
-
-	.taskbar-item.active {
-		box-shadow:
-			var(--shadow-sm),
-			0px 15px 20px -15px rgba(from var(--primary-400) r g b / 0.8);
-		background-color: var(--taskbar-item-bg-color_active);
-	}
-
-	.taskbar-item.minimized {
-		opacity: 0.7;
-		background-color: transparent;
-
-		.taskbar-item-icon {
-			filter: grayscale(1);
+			color: var(--color-accent-foreground);
 		}
 	}
 </style>
--- src/routes/(app)/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
-</script>
-
-<Button>Button</Button>
-<Input />
