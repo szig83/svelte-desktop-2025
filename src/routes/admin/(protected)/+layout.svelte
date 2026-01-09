f0210d074c7696f028a9b4658d93e7255038caea
Szigeti Balázs <balazs.mindenes@gmail.com>
2026-01-09 12:32:36 +0100

    svelte warning ignore

--- src/routes/admin/(protected)/+layout.svelte
+++ src/routes/admin/(protected)/+layout.svelte
@@ -1,6 +1,6 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
-    import '../appAdmin.css';
+	import '../appAdmin.css';
 	import './protected.css';
 	import Desktop from '$lib/components/core/Desktop.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
@@ -10,6 +10,9 @@
 	let { children, data } = $props();
 
 	// Settings kontextus beállítása, hogy a gyerek komponensek elérjék
+	// A svelte-ignore azért kell, mert a data változóra false pozitív warningot ad a svelte,
+	// mert nem tudja, hogy az a localsból jön és csak oldalbetöltéskor frissül.
+	// svelte-ignore state_referenced_locally
 	setContext('settings', data.settings);
 </script>
 
